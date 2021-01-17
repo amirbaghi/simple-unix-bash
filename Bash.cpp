937b356ac6e607f70dd3c930b2c854653ab3eee2
amirbaghi <bamirmasoud@gmail.com>
2021-01-17 15:43:41 +0330

    fix bugs in foreground execution

--- Bash.cpp
+++ Bash.cpp
@@ -1,22 +1,61 @@
 #include "Bash.h"
 #include <regex>
-#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 using namespace std;
 
-void Bash::extract_arguments(string &input, vector<std::string> &args)
+void Bash::extract_arguments(string &input, vector<string> &args)
 {
     string temp;
+    args.clear();
 
     regex delims("[^ \t\n]+");
     auto tokens_begin = sregex_iterator(input.begin(), input.end(), delims);
     auto tokens_end = sregex_iterator();
 
-    for (sregex_iterator i = tokens_begin ; i != tokens_end ; i++)
+    for (sregex_iterator i = tokens_begin; i != tokens_end; i++)
     {
         args.push_back((*i).str());
     }
 
+}
+
+void Bash::fg_exec(vector<std::string> &args)
+{
+
+    // Making a vector of char pointers from the vector of strings
+    std::vector<char *> argv;
+    argv.reserve(args.size());
+
+    for (int i = 0; i < args.size(); ++i)
+        argv.push_back(const_cast<char *>(args[i].c_str()));
+
+    argv.push_back(NULL);
+
+
+    int status;
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        cout << "ERROR: Forking Process Failed" << endl;
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        status = execvp(argv[0], &argv[0]);
+        if (status < 0)
+        {
+            cout << "ERROR: Executing Failed" << endl;
+            exit(1);
+        }
+    }
+    else
+    {
+        while (wait(NULL) != pid)
+            ;
+    }
 }
 
 void Bash::bash()
@@ -35,9 +74,14 @@ void Bash::bash()
         {
             exit(0);
         }
-        cout << endl;
+        fg_exec(args);
 
-    }    
+        cout << endl;
+    }
 }
 
-
+int main(int argc, char **argv)
+{
+    Bash::bash();
+    return 0;
+}
