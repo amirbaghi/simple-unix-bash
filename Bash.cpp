8d994863bde2282cfaa9657b4bfbb344ddacc6a0
amirbaghi <bamirmasoud@gmail.com>
2021-01-17 13:56:54 +0330

    initial commit

---
+++ Bash.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#ifndef BASH_MINE_H
+#define BASH_MINE_H
+
+namespace Bash
+{
+
+    // Main function responsible for getting input and calling appropriate functions
+    void bash();
+
+    // Function responsible for extracting arguments from input into a vector of strings
+    std::vector<std::string> extract_arguments(std::string input);
+
+    // Function responsible for executing foreground programs
+    void fg_exec(std::vector<std::string> args);
+
+
+}; // namespace Bash
+
+#endif
